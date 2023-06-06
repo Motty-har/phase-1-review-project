625dc4d463af8f3a30323609d13eba973f0cfb3a
Motty-har <harligmotty@gmail.com>
2023-06-06 10:55:41 -0400

    added rest of fetch request

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const form = document.getElementById("get")
-document.addEventListener("DOMContentLoaded", form.addEventListener("click", (event) => {
+const form = document.getElementById("form")
+document.addEventListener("DOMContentLoaded", form.addEventListener("submit", (event) => {
     event.preventDefault()
     const name = document.getElementById("name").value
     console.log(name)
-fetch(`http://localhost:3000/roster/}`)
+fetch(`http://localhost:3000/roster/${name}`)
 .then(resp => resp.json()
 )
-.then(obj => console.log(obj))
+.then(data => {
+    const name = document.getElementById("playerName")
+    const number = document.getElementById("playerNumber")
+    const position = document.getElementById("playerPosition")
+    name.innerText = data.fullName
+    number.innerText = data.jerseyNumber
+    position.innerText = data.position.name
+})
 }
 ))
