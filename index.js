1adfa4627fb147fb0264fec37f8ea8b9cf39e82e
Motty-har <harligmotty@gmail.com>
2023-06-06 09:42:43 -0400

    created fetch request, need to figure out how to fetch accurate data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const form = document.getElementById("get")
 document.addEventListener("DOMContentLoaded", form.addEventListener("click", (event) => {
     event.preventDefault()
-    console.log("Clicked")
+    const name = document.getElementById("name").value
+    console.log(name)
+fetch(`http://localhost:3000/roster/}`)
+.then(resp => resp.json()
+)
+.then(obj => console.log(obj))
 }
 ))
