9982bf594d6a4320adf678557709d4a988549abc
Motty-har <harligmotty@gmail.com>
2023-06-06 11:39:32 -0400

    fixed fetch request that it can be searched by player name not id

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const form = document.getElementById("form")
 document.addEventListener("DOMContentLoaded", form.addEventListener("submit", (event) => {
     event.preventDefault()
     const name = document.getElementById("name").value
-    console.log(name)
-fetch(`http://localhost:3000/roster/${name}`)
+fetch(`http://localhost:3000/roster?fullName=${name}`)
 .then(resp => resp.json()
 )
 .then(data => {
+    const obj = data[0]
     const name = document.getElementById("playerName")
     const number = document.getElementById("playerNumber")
     const position = document.getElementById("playerPosition")
-    name.innerText = data.fullName
-    number.innerText = data.jerseyNumber
-    position.innerText = data.position.name
+    name.innerText = obj.fullName
+    number.innerText = obj.jerseyNumber
+    position.innerText = obj.position.name
 })
 }
 ))
