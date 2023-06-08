b5e9c1ff15c45fb97ba2a17ef0a10ae8cb9fda5a
Motty-har <harligmotty@gmail.com>
2023-06-08 04:14:36 -0400

    made funcitons single responsibility

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ document.addEventListener("DOMContentLoaded", form.addEventListener("submit",
         const name = document.getElementById("name").value
         fetch(`http://localhost:3000/roster?fullName=${name}`)
         .then(resp => resp.json())
-        .then(data => {
-            const obj = data[0]
-            const name = document.getElementById("playerName")
-            const number = document.getElementById("playerNumber")
-            const position = document.getElementById("playerPosition")
-            name.innerText = obj.fullName
-            number.innerText = obj.jerseyNumber
-            position.innerText = obj.position.name
-        })
+        .then(data => fetchPlayer(data))
     }
 ))
+
+function fetchPlayer(data) {
+    const obj = data[0]
+    const name = document.getElementById("playerName")
+    const number = document.getElementById("playerNumber")
+    const position = document.getElementById("playerPosition")
+    name.innerText = obj.fullName
+    number.innerText = obj.jerseyNumber
+    position.innerText = obj.position.name
+}
 
 const positionSearch = document.getElementById("positionForm")
 
@@ -26,21 +28,22 @@ positionSearch.addEventListener("click",
     fetch("http://localhost:3000/roster")
     .then(resp => resp.json())
     .then(data => {
-        const position = document.getElementById("position").value
-        data.forEach((data) => {
-            if(position === data.position.name){
-                const posDiv = document.getElementById('posInfo')
-                const posName = document.createElement('ul')
-                const posNumber = document.createElement('ul')
-                const br = document.createElement('br')
-                posName.innerText = data.fullName
-                posNumber.innerText = data.jerseyNumber
-                posDiv.append(posName, posNumber, br)
-            }
-        })
+        data.forEach((data) => playerByPos(data))
     })
 })
 
+function playerByPos(data){
+    const position = document.getElementById("position").value
+    if(position === data.position.name){
+        const posDiv = document.getElementById('posInfo')
+        const posName = document.createElement('ul')
+        const posNumber = document.createElement('ul')
+        const br = document.createElement('br')
+        posName.innerText = data.fullName
+        posNumber.innerText = data.jerseyNumber
+        posDiv.append(posName, posNumber, br)
+    }
+}
 const header = document.getElementById('header')
 
 header.addEventListener("mouseover", (event) => {
